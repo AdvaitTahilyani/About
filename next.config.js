ae1e972169bba3c7cb526d26c3fb88755b4aae6a
Advait Tahilyani <advaitt3@illinois.edu>
2025-08-20 10:54:09 +0530

    Changed production json

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    trailingSlash: true,
-    images: {
-        unoptimized: true
-    },
-    distDir: 'out'
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig
